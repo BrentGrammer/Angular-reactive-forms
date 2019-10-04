d9ad3829d733662c8d814fbfbd97f6324ad4abe4
Brent Marquez <brentonmarquez@gmail.com>
2019-10-03 21:44:35 -0600

    intitial form setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FormGroup, FormControl } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent {
-  genders = ['male', 'female'];
+export class AppComponent implements OnInit {
+  genders = ["male", "female"];
+  signupForm: FormGroup;
+
+  ngOnInit() {
+    /*eslint quote-props: ["error", "as-needed"]*/
+    /*eslint-env es6*/
+    // It's a good idea to wrap keys in quotes to prevent mangling during minification since html references them by name
+    this.signupForm = new FormGroup({
+      username: new FormControl(null),
+      email: new FormControl(null),
+      gender: new FormControl("male") // default select value
+    });
+  }
 }
