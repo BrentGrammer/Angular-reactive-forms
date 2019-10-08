26ec49028116067e301a222bc470b73953b8863c
Brent Marquez <brentonmarquez@gmail.com>
2019-10-07 21:59:04 -0600

    add custom validator to component form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
 
 @Component({
   selector: "app-root",
@@ -9,6 +9,7 @@ import { FormGroup, FormControl, Validators } from "@angular/forms";
 export class AppComponent implements OnInit {
   genders = ["male", "female"];
   signupForm: FormGroup;
+  forbiddenUsernames = ["Chris", "Anna"];
 
   ngOnInit() {
     /*eslint quote-props: ["error", "as-needed"]*/
@@ -16,14 +17,38 @@ export class AppComponent implements OnInit {
     // It's a good idea to wrap keys in quotes to prevent mangling during minification since html references them by name
     this.signupForm = new FormGroup({
       userData: new FormGroup({
-        username: new FormControl(null, Validators.required),
+        username: new FormControl(null, [
+          Validators.required,
+          this.forbiddenNames.bind(this)
+        ]),
         email: new FormControl(null, [Validators.required, Validators.email])
       }),
-      gender: new FormControl("male") // default select value
+      gender: new FormControl("male"), // default select value
+      hobbies: new FormArray([])
     });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+  }
+
+  onAddHobby() {
+    const control = new FormControl(null, Validators.required);
+    // You need to cast the returned value as a FormArray as shown below:
+    (<FormArray>this.signupForm.get("hobbies")).push(control);
+  }
+
+  getHobbyControls() {
+    // This is necessary for Angular 8 - you can't use signupForm.get in the template to get the Form Array
+    return (this.signupForm.get("hobbies") as FormArray).controls;
+  }
+
+  forbiddenNames(control: FormControl): { [key: string]: boolean } {
+    // checks if value of control is included on the forbidden names array
+    // Note: -1 returns true, so you need to check that indexOf result is not equal to -1
+    if (this.forbiddenUsernames.indexOf(control.value) !== -1) {
+      return { nameIsForbidden: true };
+    }
+    return null;
   }
 }
